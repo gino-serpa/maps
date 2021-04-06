78e7934f14303987f4a42fe5651636c1e74386ba
gino <gino.serpa@gmail.com>
2021-04-06 12:20:04 -0400

    trying to automate bounding boxes

--- geo_maps.py
+++ geo_maps.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 import matplotlib.pyplot as plt
 import tilemapbase
+from pyproj import Proj, transform
 
 def create_map(state, points_gpd):
 
@@ -25,12 +26,32 @@ def create_map(state, points_gpd):
     tilemapbase.init(create=True)
     extent = tilemapbase.extent_from_frame(state_gpd, buffer = 25)
 
+    # Read bounding boxes
+    state_bb_file = 'states_bounding_boxes.csv'
+    states_bb_df = pd.read_csv(state_bb_file)
+    state_bb_df = states_bb_df[states_bb_df['NAME']==state]
+    x_min = list(state_bb_df.xmin)[0]
+    x_max = list(state_bb_df.xmax)[0]
+    y_min = list(state_bb_df.ymin)[0]
+    y_max = list(state_bb_df.ymax)[0]
+    print(x_min,x_max)
+    print(y_min,y_max)
+    outProj = Proj(init='epsg:3857')
+    inProj = Proj(init='epsg:4326')
+    x_min,y_min = transform(inProj, outProj, x_min, y_min)
+    x_max,y_max = transform(inProj, outProj, x_min, y_min)
+    print(x_min,x_max)
+    print(y_min,y_max)
+
+    bounding_box = [x_min, x_max, y_min, y_max]
+
     fig, ax = plt.subplots(figsize=(15,15))
     plotter = tilemapbase.Plotter(extent, tilemapbase.tiles.build_OSM(), width=1000)
     plotter.plot(ax)
 
-    #ax.set_xlim(bounding_box[0]+2000, bounding_box[1])
-    #ax.set_ylim(bounding_box[2]+2000, bounding_box[3])
+    # Bounding box
+    ax.set_xlim(bounding_box[0]+2000, bounding_box[1])
+    ax.set_ylim(bounding_box[2]+2000, bounding_box[3])
 
     state_gpd.plot(ax=ax, alpha=0.3, edgecolor="black", facecolor="white")
     points_gpd.plot(ax=ax, alpha = 0.4, color="red", marker='$\\bigtriangledown$',)
